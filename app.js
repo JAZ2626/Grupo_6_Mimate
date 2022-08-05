e6058b562dff21e256dbf7ccd4cb97360ab355f6
JanaMGoro <janateit@gmail.com>
2022-08-05 00:46:03 -0300

    actualice

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 app.listen(3030, ()=>{
     console.log(" ");
 })
-app.use(express.static('public'));git 
+app.use(express.static('public'));
 app.get('/', (req, res)=>{
     res.sendFile(path.resolve(__dirname, "./views/index.html"))
 });
