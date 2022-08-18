01c0e69b8fec6272a3b1b798a0bf983a02574ec7
JAZ <jazromano2626@gmail.com>
2022-08-17 21:18:17 -0300

    subi mas fotos

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ app.get('/', (req, res)=>{
     res.sendFile(path.resolve(__dirname, "./views/index.html"))
 });
 
+app.get('/home', (req, res)=>{
+    res.sendFile(path.resolve(__dirname, "./views/home.html"))
+});
+
 app.get('/login', (req, res)=>{
     res.sendFile(path.resolve(__dirname, "./views/login.html"))
 });
