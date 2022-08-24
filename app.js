8531cee634eac5836fd8dd28fb7a282414f218fe
JanaMGoro <janateitel@gmail.com>
2022-08-24 02:04:45 -0300

    actualice

--- app.js
+++ app.js
@@ -10,9 +10,6 @@ app.get('/', (req, res)=>{
     res.sendFile(path.resolve(__dirname, "./views/index.html"))
 });
 
-app.get('/home', (req, res)=>{
-    res.sendFile(path.resolve(__dirname, "./views/home.html"))
-});
 
 app.get('/login', (req, res)=>{
     res.sendFile(path.resolve(__dirname, "./views/login.html"))
@@ -22,13 +19,12 @@ app.get('/productCart', (req, res)=>{
     res.sendFile(path.resolve(__dirname, "./views/productCart.html"))
 });
 
+
+
 app.get('/productDetail', (req, res)=>{
     res.sendFile(path.resolve(__dirname, "./views/productDetail.html"))
 });
 
-app.get('/mio', (req, res)=>{
-    res.sendFile(path.resolve(__dirname, "./views/mio.html"))
-});
 
 app.get('/register', (req, res)=>{
     res.sendFile(path.resolve(__dirname, "./views/register.html"))
