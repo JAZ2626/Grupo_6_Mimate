10a11cccd50053c05f2c5307af098c4cbc060a24
menuteitel <menuteitel@gmail.com>
2022-09-05 12:31:04 -0300

    arreglo

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,33 +1,38 @@
 const express = require('express');
 
 const productList = [
-    { productName: "Limpieza Facial",
-      price: 4500,
-    //   img: "../img/1.png",
-      detail: " Lorem ipsum dolor sit amet consectetur adipisicing elit."
+    {
+        productName: "Limpieza Facial",
+        price: 4500,
+        //   img: "../img/1.png",
+        detail: " Lorem ipsum dolor sit amet consectetur adipisicing elit."
     },
-    { productName: "Masajes Corporales",
-      price: 8000,
-    //   img: "../img/5.png",
-      detail: " Lorem ipsum dolor sit amet consectetur adipisicing elit."
+    {
+        productName: "Masajes Corporales",
+        price: 8000,
+        //   img: "../img/5.png",
+        detail: " Lorem ipsum dolor sit amet consectetur adipisicing elit."
     },
-    { productName: "Maquillaje",
-      price: 3000,
-    //   img: "../img/3.png",
-      detail: " Lorem ipsum dolor sit amet consectetur adipisicing elit."
+    {
+        productName: "Maquillaje",
+        price: 3000,
+        //   img: "../img/3.png",
+        detail: " Lorem ipsum dolor sit amet consectetur adipisicing elit."
     },
-    { productName: "Masajes Sueves",
-      price: 4000,
-    //   img: "../img/4.png",
-      detail: " Lorem ipsum dolor sit amet consectetur adipisicing elit."
+    {
+        productName: "Masajes Sueves",
+        price: 4000,
+        //   img: "../img/4.png",
+        detail: " Lorem ipsum dolor sit amet consectetur adipisicing elit."
     },
 ]
 
 const controller = {
 
     productDetail: (req, res) => {
-        const id = req.params.id-1
+        const id = req.params.id - 1
         const productI = productList[id];
+        console.log(productI);
         res.render('productDetail', {
             nombre: productI.productName,
             precio: productI.price,
@@ -38,7 +43,7 @@ const controller = {
     // productId: (req, res) => {
     //     res.render('productDetail', )
     // },
-    
+
     addProduct: (req, res) => {
         res.render('addProduct')
     },
