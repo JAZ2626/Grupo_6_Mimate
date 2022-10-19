22bbc740785680568aef6b788de032031fa5314e
menuteitel <menuteitel@gmail.com>
2022-10-19 10:49:50 -0400

    validation

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const { validationResult } = require('express-validator');
+
 
 const controller = {
 register:  (req, res)=>{
@@ -6,7 +8,15 @@ register:  (req, res)=>{
 },
 
 login: (req, res)=>{
-    res.render('login')
+    let errors = validationResult(req);
+    if (errors.isEmpty()){
+        let user = req.body;
+        userId = usersModel.create(user);
+        res.redirect('/' + userId)
+    }else{
+        return res.render('login', {errors: errors.mapped(),
+            old: req.body});
+    }
 },
 
 productCart: (req, res)=>{
--- src/routers/user.js
+++ src/routers/user.js
@@ -2,6 +2,7 @@ const express = require('express');
 const controller = require('../controllers/userController');
 const multer = require('multer');
 const router = express.Router();
+const { body } = require('express-validator');
 
 const storage = multer.diskStorage({
     destination: function(req, file, cb){
@@ -15,9 +16,16 @@ const storage = multer.diskStorage({
 
 const upload = multer({storage: storage});
 
+const validationLog = [
+    body('email').notEmpty().withMessage('Debes completar el nombre').bail()
+    .isEmail().withMessage('Debes escribir un email valido'),
+    body('password').notEmpty().withMessage('Debes completar la contraseña').bail()
+    .isLength({ min: 6 }).withMessage('La contraseña debe tener al menos 6 caracteres'),
+];
+
 router.get('/register', upload.single('image'), controller.register);
 
-router.get('/login', controller.login);
+router.get('/login', validationLog, controller.login);
 
 router.get('/productCart', controller.productCart);
 
